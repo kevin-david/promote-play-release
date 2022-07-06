f82ab8c6218839379943667de4fea02c5855de38
Kevin David <kevin-david@github.com>
2022-07-06 17:35:42 +0000

    Porting changes from boswelja/promote-play-beta-action#71
    I had enough with fighting type errors and decided to rewrite this in typescript.

--- README.md
+++ README.md
@@ -1,105 +1,30 @@
-<p align="center">
-  <a href="https://github.com/actions/typescript-action/actions"><img alt="typescript-action status" src="https://github.com/actions/typescript-action/workflows/build-test/badge.svg"></a>
-</p>
+Typescript fork of https://github.com/boswelja/promote-play-beta-action
 
-# Create a JavaScript Action using TypeScript
+# promote-play-beta-action
+Promote the latest open testing release from one track to another on Google Play.
 
-Use this template to bootstrap the creation of a TypeScript action.:rocket:
+## Inputs
 
-This template includes compilation support, tests, a validation workflow, publishing, and versioning guidance.  
+### `service-account-json-raw`
 
-If you are new, there's also a simpler introduction.  See the [Hello World JavaScript Action](https://github.com/actions/hello-world-javascript-action)
+**Required:** The raw json text of the service account private key to authorize the upload request.
 
-## Create an action from this template
+### `package-name`
 
-Click the `Use this Template` and provide the new repo details for your action
+**Required:** The package name, or Application Id, of the app you are editing.
 
-## Code in Main
+### `inapp-update-priority`
 
-> First, you'll need to have a reasonably modern version of `node` handy. This won't work with versions older than 9, for instance.
+In-app update priority of the release. All newly added APKs in the release will be considered at this priority. Can take values in the range [0, 5], with 5 the highest priority. Defaults to 0.
 
-Install the dependencies  
-```bash
-$ npm install
-```
+### `user-fraction`
 
-Build the typescript and package it for distribution
-```bash
-$ npm run build && npm run package
-```
+Portion of users who should get the staged version of the app. Accepts values between 0.0 and 1.0 (exclusive-exclusive).
 
-Run the tests :heavy_check_mark:  
-```bash
-$ npm test
+### `from-track`
 
- PASS  ./index.test.js
-  ✓ throws invalid number (3ms)
-  ✓ wait 500 ms (504ms)
-  ✓ test runs (95ms)
+The source track to promote from. Defaults to beta.
 
-...
-```
+### `to-track`
 
-## Change action.yml
-
-The action.yml defines the inputs and output for your action.
-
-Update the action.yml with your name, description, inputs and outputs for your action.
-
-See the [documentation](https://help.github.com/en/articles/metadata-syntax-for-github-actions)
-
-## Change the Code
-
-Most toolkit and CI/CD operations involve async operations so the action is run in an async function.
-
-```javascript
-import * as core from '@actions/core';
-...
-
-async function run() {
-  try { 
-      ...
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run()
-```
-
-See the [toolkit documentation](https://github.com/actions/toolkit/blob/master/README.md#packages) for the various packages.
-
-## Publish to a distribution branch
-
-Actions are run from GitHub repos so we will checkin the packed dist folder. 
-
-Then run [ncc](https://github.com/zeit/ncc) and push the results:
-```bash
-$ npm run package
-$ git add dist
-$ git commit -a -m "prod dependencies"
-$ git push origin releases/v1
-```
-
-Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
-
-Your action is now published! :rocket: 
-
-See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
-
-## Validate
-
-You can now validate the action by referencing `./` in a workflow in your repo (see [test.yml](.github/workflows/test.yml))
-
-```yaml
-uses: ./
-with:
-  milliseconds: 1000
-```
-
-See the [actions tab](https://github.com/actions/typescript-action/actions) for runs of this action! :rocket:
-
-## Usage:
-
-After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and latest V1 action
+The target track to promote to. Defaults to production.
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,113 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as fs from 'fs'
+import { google, androidpublisher_v3 } from 'googleapis'
+
+let serviceAccountFile = './serviceAccountJson.json'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const packageName = core.getInput('package-name', { required: true })
+    const rawServiceAccountJson = core.getInput('service-account-json-raw', {
+      required: true
+    })
+    const inAppUpdatePriority = parseInt(core.getInput('inapp-update-priority'))
+    const userFraction = parseFloat(core.getInput('user-fraction'))
+    const fromTrack = core.getInput('from-track')
+    const toTrack = core.getInput('to-track')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(
+      'Writing service account JSON to file, setting env variable so auth can find it'
+    )
+    await fs.promises.writeFile(serviceAccountFile, rawServiceAccountJson, {
+      encoding: 'utf8'
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    serviceAccountFile = await fs.promises.realpath(serviceAccountFile)
+    core.exportVariable('GOOGLE_APPLICATION_CREDENTIALS', serviceAccountFile)
+
+    core.debug('Authenticating: creating auth client')
+    const auth = new google.auth.GoogleAuth({
+      scopes: ['https://www.googleapis.com/auth/androidpublisher']
+    })
+    const authClient = await auth.getClient()
+
+    const publisher: androidpublisher_v3.Androidpublisher =
+      google.androidpublisher('v3')
+    core.info('Creating a new app edit')
+    const appEdit = await publisher.edits.insert({
+      packageName,
+      auth: authClient
+    })
+
+    const appEditId = appEdit.data.id
+    if (!appEditId) {
+      throw new Error(`appEditId is not defined. Data: ${appEdit.data}`)
+    }
+
+    core.info(`Getting current ${fromTrack} info`)
+    const sourceTrack = await publisher.edits.tracks.get({
+      auth: authClient,
+      packageName,
+      editId: appEditId,
+      track: fromTrack
+    })
+
+    core.debug(`Mapping ${fromTrack} releases to target releases`)
+    core.debug(
+      `Applying new fraction ${userFraction}, new priority ${inAppUpdatePriority}`
+    )
+
+    const sourceReleases = sourceTrack.data.releases
+    if (!sourceReleases) {
+      throw new Error(
+        `sourceReleases is not defined. Data: ${sourceTrack.data}`
+      )
+    }
+
+    const toTrackReleases = sourceReleases.map(release => {
+      release.inAppUpdatePriority = inAppUpdatePriority
+      release.userFraction = userFraction
+      // See https://developers.google.com/android-publisher/api-ref/rest/v3/edits.tracks#status
+      // The API requires us to explicitly set this for some reason
+      // Otherwise, you get an error: `Error: Release status must be specified.`
+      release.status = userFraction === 1.0 ? 'completed' : 'inProgress'
+      return release
+    })
+
+    core.info(`Switching ${fromTrack} release to ${toTrack}`)
+    await publisher.edits.tracks.update({
+      auth: authClient,
+      editId: appEditId,
+      track: toTrack,
+      packageName,
+      requestBody: {
+        track: toTrack,
+        releases: toTrackReleases
+      }
+    })
+
+    core.info('Committing changes')
+    const commitResult = await publisher.edits.commit({
+      auth: authClient,
+      editId: appEditId,
+      packageName
+    })
+
+    // Check commit was successful
+    if (!commitResult.data.id) {
+      throw new Error(
+        `Error ${commitResult.status}: ${commitResult.statusText}`
+      )
+    }
+
+    core.info(`Successfully promoted release to ${toTrack}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+export async function cleanup(): Promise<void> {
+  await fs.promises.unlink(serviceAccountFile)
 }
 
 run()
